95dd13f3e7d5ba0eb27879dfbe9e78a74064baf9
mrkuncen89-ui <mrkuncen89@gmail.com>
2026-04-25 22:53:04 +0700

    Initialize version configuration in version.php

---
+++ config/version.php
@@ -0,0 +1,4 @@
+<?php
+define('APP_VERSION',      '1.0.5');
+define('APP_RELEASE_DATE', '25 April 2026');
+define('VERSION_URL',      'https://raw.githubusercontent.com/mrkuncen89-ui/CBT-TKA-Kecamatan/main/version.json');
